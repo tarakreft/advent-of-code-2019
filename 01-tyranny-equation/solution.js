a1030945dc147fa98b5f392aeeb3743f941862e2
Andrew Beers <andybeers@gmail.com>
2019-12-05 21:17:14 -0800

    add readfile utils, test calculate fuel for mass

--- 01-tyranny-equation/solution.js
+++ 01-tyranny-equation/solution.js
@@ -1,1 +1,13 @@
 // --- Day 1: The Tyranny of the Rocket Equation ---
+
+// take its mass, divide by three, round down, and subtract 2
+function calculateFuelByMass(mass) {
+  const roundedValue = Math.floor(mass / 3)
+  return roundedValue >= 1 ? roundedValue - 2 : 0
+}
+
+// tests
+console.log(calculateFuelByMass(12), ' ===== 2')
+console.log(calculateFuelByMass(14), ' ===== 2')
+console.log(calculateFuelByMass(1969), ' ===== 654')
+console.log(calculateFuelByMass(100756), ' ===== 33583')

0e7742f617c7ad85e522240a8a765b2f75b3a2ba
Andrew Beers <andybeers@gmail.com>
2019-12-05 21:42:04 -0800

    Import and store data; derive sum fuel solution

--- 01-tyranny-equation/solution.js
+++ 01-tyranny-equation/solution.js
@@ -1,8 +1,9 @@
 // --- Day 1: The Tyranny of the Rocket Equation ---
+const { readFileAndSplitNewline } = require('../utils/data-files')
 
 // take its mass, divide by three, round down, and subtract 2
 function calculateFuelByMass(mass) {
-  const roundedValue = Math.floor(mass / 3)
+  const roundedValue = Math.floor(parseInt(mass) / 3)
   return roundedValue >= 1 ? roundedValue - 2 : 0
 }
 
@@ -11,4 +12,10 @@ function calculateFuelByMass(mass) {
 // console.log(calculateFuelByMass(14), ' ===== 2')
 // console.log(calculateFuelByMass(1969), ' ===== 654')
 // console.log(calculateFuelByMass(100756), ' ===== 33583')
+
+const moduleMassData = readFileAndSplitNewline('./andyData.txt')
+const totalFuel = moduleMassData.reduce((total, moduleMass) => total + calculateFuelByMass(moduleMass), 0)
+
+console.log(totalFuel)
+
 
